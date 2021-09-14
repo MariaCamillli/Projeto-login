16c6bba2bef321c93bda784240c1a85580a99db3
MariaCamillli <90351670+MariaCamillli@users.noreply.github.com>
2021-09-14 16:27:18 -0300

    Login.css

---
+++ Login.css
@@ -0,0 +1,8 @@
+body{
+    background-image:
+      url(https://i.pinimg.com/564x/5e/a3/c3/5ea3c3499d59dcc3e5caa5b4c8d1c2ce.jpg);
+   background-attachment: fixed;
+   background-size:cobver;
+   background-position: center center;
+    }
+font-family: 'Rampart One', cursive;
